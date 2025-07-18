52581b8cb9bf28cace38e84c92b8ff18bc233828
Omar Velasco <oteran4812@gmail.com>
2025-07-18 12:05:53 -0400

    subiendo correcciones, incluyendo imagen en quienes somos y realizando consultorias

--- js/consulting.js
+++ js/consulting.js
@@ -55,3 +55,25 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 });
+
+// Inicializar Swiper
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
